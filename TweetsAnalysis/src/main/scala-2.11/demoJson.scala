88d801e6f19fa4722c0d77d458a7fb733f16c52b
atanumaiti16 <atanumaiti16@gmail.com>
2017-03-07 20:24:05 -0600

    Update demoJson.scala

--- TweetsAnalysis/src/main/scala-2.11/demoJson.scala
+++ TweetsAnalysis/src/main/scala-2.11/demoJson.scala
@@ -1,5 +1,5 @@
 /**
-  * Created by monica on 10/22/16.
+  * Created by Atanu on 10/22/16.
   */
 
 
@@ -17,7 +17,7 @@ object demoJson {
     val sqlContext = new SQLContext(sc)
 
     //RDD-1 :
-    val tweettable =  sqlContext.read.json ("/home/monica/Documents/PBPhase2/CleanElections.json")
+    val tweettable =  sqlContext.read.json ("/home/atanu/Documents/PBPhase2/CleanElections.json")
     tweettable.createTempView("election")
     val tweets = sqlContext.sql("SELECT id, user.id, text FROM election WHERE id IS NOT NULL").rdd
     val mentionHillary= tweets.filter( row=>
@@ -27,7 +27,7 @@ object demoJson {
 
     mentionHillary.map(row =>
     row.getLong(0)+","+row.getLong(1)
-    ).saveAsTextFile("/home/monica/Documents/PBPhase2/RDD1/RDD1a_MentionHillary")
+    ).saveAsTextFile("/home/atanu/Documents/PBPhase2/RDD1/RDD1a_MentionHillary")
     println(mentionHillary.count()) //output: 24881
 
     val mentionTrump= tweets.filter( row=>
@@ -35,50 +35,51 @@ object demoJson {
         )
     mentionTrump.map(row =>
       row.getLong(0)+","+row.getLong(1)
-    ).saveAsTextFile("/home/monica/Documents/PBPhase2/RDD1/RDD1a_MentionTrump")
+    ).saveAsTextFile("/home/atanu/Documents/PBPhase2/RDD1/RDD1a_MentionTrump")
     println(mentionTrump.count()) //output: 35075
-    // collect().fo.write.format("com.databricks.spark.csv").save("/home/monica/Documents/PBPhase2/RDD1"
+    // collect().fo.write.format("com.databricks.spark.csv").save("/home/atanu/Documents/PBPhase2/RDD1"
 
 
   //DATAFRAME - 1:
-//    val hashtags = sqlContext.sql("SELECT entities.hashtags.text as hashtags,COUNT(entities.hashtags.text) as count " +
-//      " FROM election WHERE entities.hashtags.text  IS NOT NULL GROUP BY entities.hashtags.text "+
-//      "ORDER BY COUNT(entities.hashtags.text) DESC " )
-//
-//   // val trendingHash = sqlContext.read.parquet("/home/monica/Documents/PBPhase2/trendinghashtags.parquet")
-//    val filteredHash = hashtags.filter(
-//      row => (row.getAs[scala.collection.mutable.WrappedArray[String]]("hashtags").length != 0)
-//    ).select("hashtags").take(5)
-//
+    val hashtags = sqlContext.sql("SELECT entities.hashtags.text as hashtags,COUNT(entities.hashtags.text) as count " +
+      " FROM election WHERE entities.hashtags.text  IS NOT NULL GROUP BY entities.hashtags.text "+
+      "ORDER BY COUNT(entities.hashtags.text) DESC " )
+
+   val trendingHash = sqlContext.read.parquet("/home/atanu/Documents/PBPhase2/trendinghashtags.parquet")
+    val filteredHash = hashtags.filter(
+      row => (row.getAs[scala.collection.mutable.WrappedArray[String]]("hashtags").length != 0)
+    ).select("hashtags").take(5)
+
 //  //  val trend = filteredHash.map(r => r.getAs[String]("hashtags"))
-//    val listHash =   filteredHash.flatMap(r=>{
-//      val ff=r.get(0).toString.replace("WrappedArray(","")
-//      val str=ff.replace(")","")
-//      val space = str.replace(" ", "")
-//        val s=space.split(",")
+      val listHash =   filteredHash.flatMap(r=>{
+      val ff=r.get(0).toString.replace("WrappedArray(","")
+      val str=ff.replace(")","")
+      val space = str.replace(" ", "")
+        val s=space.split(",")
 //      s
-//    }).toList.distinct
-//    listHash.foreach(println)
-//
-//
-//    val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/monica/Documents/PBPhase2/DF1/trendinghashtags.txt",true)))
-//    for (x<- listHash) {
-//      writer.write(x + "\n")
-//    }
-//    writer.close()
-//
-//    //RDD-2: location
-//    val location= sqlContext.sql("SELECT user.location, user.id, text FROM election WHERE user.location IS NOT null").rdd
-//    val locTweets= location.filter( row=>
-//      (row.getString(2).toLowerCase.contains("donald") || row.getString(2).toLowerCase.contains("trump")
-//        || row.getString(2).toLowerCase.contains("hillary")
-//        || row.getString(2).toLowerCase.contains("clinton")
-//        || row.getString(2).toLowerCase.contains("makeamericagreatagain")
-//        || row.getString(2).toLowerCase.contains("vote"))
-//    )
-//    location.take(50).foreach(println)
-//    locTweets.saveAsTextFile("/home/monica/Documents/PBPhase2/RDD2_Locations")
-//
+    }).toList.distinct
+    listHash.foreach(println)
+
+
+    val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/atanu/Documents/PBPhase2/DF1/trendinghashtags.txt",true)))
+    for (x<- listHash) {
+      writer.write(x + "\n")
+    }
+    writer.close()
+
+   //RDD-2: location
+    val location= sqlContext.sql("SELECT user.location, user.id, text FROM election WHERE user.location IS NOT null").rdd
+    val locTweets= location.filter( row=>
+      (row.getString(2).toLowerCase.contains("donald") || row.getString(2).toLowerCase.contains("trump")
+        || row.getString(2).toLowerCase.contains("hillary")
+        || row.getString(2).toLowerCase.contains("clinton")
+        || row.getString(2).toLowerCase.contains("makeamericagreatagain")
+        || row.getString(2).toLowerCase.contains("vote"))
+    )
+   
+    location.take(50).foreach(println)
+    locTweets.saveAsTextFile("/home/atanu/Documents/PBPhase2/RDD2_Locations")
+
 //
 //    val text= sqlContext.sql("SELECT entities.hashtags.text, COUNT(entities.hashtags.text)  from election  GROUP BY entities.hashtags.text ORDER BY  COUNT(entities.hashtags.text) DESC ")
 //    text.show()
@@ -87,10 +88,10 @@ object demoJson {
 //    val media_url=sqlContext.sql("SELECT   entities.media.display_url , id from election WHERE entities.media.display_url is not null ")
 //    media_url.show()
 //    //trending media urls
-//    val media_url2=sqlContext.sql("SELECT  entities.media.display_url, count(entities.media.display_url) as CountMediaEntities  from election GROUP BY entities.media.display_url ORDER BY COUNT(entities.media.display_url) DESC")
-//    media_url2.show()
-//    media_url.write.save("/home/monica/Documents/PBPhase2/DF2/mediaUrl")
-//    media_url2.write.save("/home/monica/Documents/PBPhase2/DF2/trendingMediaUrl")
+    val media_url2=sqlContext.sql("SELECT  entities.media.display_url, count(entities.media.display_url) as CountMediaEntities  from election GROUP BY entities.media.display_url ORDER BY COUNT(entities.media.display_url) DESC")
+    media_url2.show()
+    media_url.write.save("/home/atanu/Documents/PBPhase2/DF2/mediaUrl")
+    media_url2.write.save("/home/atanu/Documents/PBPhase2/DF2/trendingMediaUrl")
 
   }
 }
